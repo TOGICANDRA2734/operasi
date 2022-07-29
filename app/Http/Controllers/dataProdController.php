0c10c35cea22c19ec3e70288e9d2a15928579105
TOGICANDRA2734 <togicandra@gmail.com>
2022-07-29 17:40:17 +0800

    Fix Percentage

--- app/Http/Controllers/dataProdController.php
+++ app/Http/Controllers/dataProdController.php
@@ -15,37 +15,111 @@ class dataProdController extends Controller
      */
     public function index()
     {
-        $record1 = DB::table('pma_dailyprod_tc')
+        /**
+         * Overburden Data
+         */
+
+        $record_OB_prod = DB::table('pma_dailyprod_tc')
             ->select(DB::raw('RIGHT(tgl,2) as prod_tgl, SUM(OB) as OB'))
             ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
             ->groupBy('tgl')
             ->orderBy('tgl')
             ->get();
 
-        $record2 = DB::table('pma_dailyprod_plan')
+        $record_OB_plan = DB::table('pma_dailyprod_plan')
             ->select(DB::raw('RIGHT(tgl,2) as prod_tgl, SUM(OB) as OB'))
             ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
             ->groupBy('tgl')
             ->orderBy('tgl')
             ->get();
 
-        $data_prod = [];
-        $data_plan = [];
+        $data_prod_ob = [];
+        $data_plan_ob = [];
 
-        foreach ($record1 as $row) {
-            $data_prod['label'][] = (int) $row->prod_tgl;
-            $data_prod['data'][] = $row->OB;
+        foreach ($record_OB_prod as $row) {
+            $data_prod_ob['label'][] = (int) $row->prod_tgl;
+            $data_prod_ob['data'][] = $row->OB;
         }
 
-        foreach ($record1 as $row) {
-            $data_plan['label'][] = (int) $row->prod_tgl;
-            $data_plan['data'][] = $row->OB;
+        foreach ($record_OB_plan as $row) {
+            $data_plan_ob['label'][] = (int) $row->prod_tgl;
+            $data_plan_ob['data'][] = $row->OB;
         }
 
-        $data_prod['chart_data_prod'] = json_encode($data_prod);
-        $data_plan['chart_data_plan'] = json_encode($data_plan);
+        $data_prod_ob['chart_data_prod_ob'] = json_encode($data_prod_ob);
+        $data_plan_ob['chart_data_plan_ob'] = json_encode($data_plan_ob);
 
-        return view('data-prod.index', $data_prod, $data_plan);
+        $data_detail_OB_prod = DB::table('pma_dailyprod_tc')
+            ->select(DB::raw('SUM(OB) as OB'))
+            ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
+            ->get();
+
+        $data_detail_OB_plan = DB::table('pma_dailyprod_plan')
+            ->select(DB::raw('SUM(OB) as OB'))
+            ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
+            ->get();
+
+
+        /**
+         * Coal Data
+         */
+        $record_coal_prod = DB::table('pma_dailyprod_tc')
+            ->select(DB::raw('RIGHT(tgl,2) as prod_tgl, SUM(coal) as coal'))
+            ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
+            ->groupBy('tgl')
+            ->orderBy('tgl')
+            ->get();
+
+        $record_coal_plan = DB::table('pma_dailyprod_plan')
+            ->select(DB::raw('RIGHT(tgl,2) as prod_tgl, SUM(coal) as coal'))
+            ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
+            ->groupBy('tgl')
+            ->orderBy('tgl')
+            ->get();
+
+        $data_prod_coal = [];
+        $data_plan_coal = [];
+
+        foreach ($record_coal_prod as $row) {
+            $data_prod_coal['label'][] = (int) $row->prod_tgl;
+            $data_prod_coal['data'][] = $row->coal;
+        }
+
+        foreach ($record_coal_plan as $row) {
+            $data_plan_coal['label'][] = (int) $row->prod_tgl;
+            $data_plan_coal['data'][] = $row->coal;
+        }
+
+        $data_prod_coal['chart_data_prod_coal'] = json_encode($data_prod_coal);
+        $data_plan_coal['chart_data_plan_coal'] = json_encode($data_plan_coal);
+
+        $data_detail_coal_prod = DB::table('pma_dailyprod_tc')
+            ->select(DB::raw('SUM(coal) as coal'))
+            ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
+            ->get();
+
+        $data_detail_coal_plan = DB::table('pma_dailyprod_plan')
+            ->select(DB::raw('SUM(coal) as coal'))
+            ->whereBetween('tgl', ['2022-07-01', '2022-07-31'])
+            ->get();
+
+
+        // Data Detail OB & Coal Per Site
+        $totalOBHarian = DB::table('pma_dailyprod_tc')
+            ->select(DB::raw('SUM(ob) totalOb, SUM(coal) totalCoal'))
+            ->whereDate('tgl', '=', DB::raw('SUBDATE(CURDATE(), 1)'))
+            ->where('kodesite', '=', 'I')
+            ->get();
+
+
+        $persenTotalObHarian = DB::table('pma_dailyprod_tc')
+        ->select(DB::raw('tgl, ob, coal'))
+        ->whereBetween('tgl', [DB::raw('SUBDATE(CURDATE(), 2)'), DB::raw('SUBDATE(CURDATE(), 1)')])
+        ->where('kodesite', '=', 'I')
+        ->get();
+
+        return view('data-prod.index', compact('data_detail_OB_prod', 'data_detail_OB_plan', 'data_prod_ob', 'data_plan_ob', 'data_detail_coal_prod', 'data_detail_coal_plan', 'data_prod_coal', 'data_plan_coal', 'totalOBHarian', 'persenTotalObHarian'));
+        // $data_prod, $data_plan
     }
 
     /**
--- resources/views/data-prod/index.blade.php
+++ resources/views/data-prod/index.blade.php
@@ -13,50 +13,48 @@
         <div class="report-card relative">
             <img class="w-20 h-auto absolute right-0 rounded-full z-10" src="{{asset('icon/mendung.GIF')}}" style="right: -2rem; top: -1.5rem;" alt="Asset">
             <a href="">
-            <div class="card border-red-100 ">
-                <div class="card-body flex flex-col border ">
-                    <!-- top -->
-                    <div class="flex flex-row justify-between items-center">
-                        <div class="flex items-center">
-                            <img class="w-10 h-10" src="{{asset('/icon_company/ABK.jpg')}}" class="h6 text-indigo-700 fad fa-shopping-cart"></img>
-                            <p class="ml-3 font-bold text-black">ABK</p>
+                <div class="card border-red-100 ">
+                    <div class="card-body flex flex-col border ">
+                        <!-- top -->
+                        <div class="flex flex-row justify-between items-center">
+                            <div class="flex items-center">
+                                <img class="w-10 h-10" src="{{asset('/icon_company/ABK.jpg')}}" class="h6 text-indigo-700 fad fa-shopping-cart"></img>
+                                <p class="ml-3 font-bold text-black">ABK</p>
+                            </div>
                         </div>
+                        <!-- end top -->
+
+                        <!-- bottom -->
+                        <div class="mt-8 ">
+                            <div class="flex justify-between items-center">
+                                <div class="flex flex-col">
+                                    <p>OB <span class="text-xs opacity-80">(bcm)</span></p>
+                                    <h4 class="font-bold text-xl">{{$totalOBHarian[0]->totalOb}}</h4>
+                                </div>
+                                <div class="rounded-full text-white badge bg-teal-400 text-xs h-5">
+                                    {{$persenTotalObHarian[1]->ob / $persenTotalObHarian[0]->ob * 100}}%
+                                    <i class="fal fa-chevron-up ml-1"></i>
+                                </div>
+                            </div>
+                            <div class="flex justify-between items-center mt-3">
+                                <div class="flex flex-col">
+                                    <p>Coal <span class="text-xs opacity-80">(mt)</span></p>
+                                    <h1 class="font-bold text-xl">{{$totalOBHarian[0]->totalCoal}}</h1>
+                                </div>
+                                <div class="rounded-full text-white badge bg-teal-400 text-xs h-5 align-middle">
+                                    {{$persenTotalObHarian[1]->coal / $persenTotalObHarian[0]->coal * 100}}%
+                                    <i class="fal fa-chevron-up ml-1"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end bottom -->
+
+
                     </div>
-                    <!-- end top -->
-
-                    <!-- bottom -->
-                    <div class="mt-8 ">
-                        <div class="flex justify-between items-center">
-                            <div class="flex flex-col">
-                                <p>OB <span class="text-xs opacity-80">(bcm)</span></p>
-                                <h1 class="font-bold text-xl num-4"></h1>
-                            </div>
-                            <div class="rounded-full text-white badge bg-teal-400 text-xs h-5">
-                                12%
-                                <i class="fal fa-chevron-up ml-1"></i>
-                            </div>
-                        </div>
-                        <div class="flex justify-between items-center mt-3">
-                            <div class="flex flex-col">
-                                <p>Coal <span class="text-xs opacity-80">(mt)</span></p>
-                                <h1 class="font-bold text-xl num-4"></h1>
-                            </div>
-                            <div class="rounded-full text-white badge bg-teal-400 text-xs h-5">
-                                12%
-                                <i class="fal fa-chevron-up ml-1"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end bottom -->
-
-
                 </div>
-            </div>
-            <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
+                <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
             </a>
         </div>
-
-        
         <!-- end card -->
 
 
@@ -320,22 +318,22 @@
             <div class="p-8 flex flex-col justify-between">
                 <div class="grid grid-cols-3 gap-3">
                     <div class="">
-                        <h2 class="h5 text-gray-700">Actual</h2>
-                        <h1 class="h2">5,337</h1>
+                        <h2 class="h6 text-gray-700">Actual</h2>
+                        <h1 class="h3">{{number_format($data_detail_OB_prod[0]->OB, 0, '.', ',')}}</h1>
                         <p class="text-black font-medium">BCM</p>
                     </div>
                     <div class="">
-                        <h2 class="h5 text-gray-700">Plan</h2>
-                        <h1 class="h2">5,337</h1>
+                        <h2 class="h6 text-gray-700">Plan</h2>
+                        <h1 class="h3">{{number_format($data_detail_OB_plan[0]->OB, 0, '.', ',')}}</h1>
                         <p class="text-black font-medium">BCM</p>
                     </div>
                     <div class="">
-                        <h2 class="h5 text-gray-700">ACH</h2>
-                        <h1 class="h2">5,337</h1>
-                        <p class="text-black font-medium">BCM</p>
+                        <h2 class="h6 text-gray-700">ACH</h2>
+                        <h1 class="h3">{{number_format($data_detail_OB_prod[0]->OB / $data_detail_OB_plan[0]->OB * 100)}}</h1>
+                        <p class="text-black font-medium">%</p>
                     </div>
                 </div>
-                
+
 
                 <a href="#" class="btn-shadow mt-6">view details</a>
             </div>
@@ -343,7 +341,7 @@
             <div class="">
                 <div class="chart-container">
                     <div class="pie-chart-container">
-                        <canvas id="pie-chart"></canvas>
+                        <canvas id="overburden"></canvas>
                     </div>
                 </div>
             </div>
@@ -383,29 +381,33 @@
             <div class="p-8 flex flex-col justify-between">
                 <div class="grid grid-cols-3 gap-3">
                     <div class="">
-                        <h2 class="h5 text-gray-700">Actual</h2>
-                        <h1 class="h2">5,337</h1>
+                        <h2 class="h6 text-gray-700">Actual</h2>
+                        <h1 class="h3">{{number_format($data_detail_coal_prod[0]->coal, 0, '.', ',')}}</h1>
                         <p class="text-black font-medium">BCM</p>
                     </div>
                     <div class="">
-                        <h2 class="h5 text-gray-700">Plan</h2>
-                        <h1 class="h2">5,337</h1>
+                        <h2 class="h6 text-gray-700">Plan</h2>
+                        <h1 class="h3">{{number_format($data_detail_coal_plan[0]->coal, 0, '.', ',')}}</h1>
                         <p class="text-black font-medium">BCM</p>
                     </div>
                     <div class="">
-                        <h2 class="h5 text-gray-700">ACH</h2>
-                        <h1 class="h2">5,337</h1>
-                        <p class="text-black font-medium">BCM</p>
+                        <h2 class="h6 text-gray-700">ACH</h2>
+                        <h1 class="h3">{{number_format($data_detail_coal_prod[0]->coal / $data_detail_coal_plan[0]->coal * 100)}}</h1>
+                        <p class="text-black font-medium">%</p>
                     </div>
                 </div>
-                
+
 
                 <a href="#" class="btn-shadow mt-6">view details</a>
 
             </div>
 
             <div class="">
-                <div id="SummaryChart"></div>
+                <div class="chart-container">
+                    <div class="pie-chart-container">
+                        <canvas id="coal"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- end body -->
@@ -414,59 +416,111 @@
 </div>
 
 <!-- javascript -->
- 
+
 <script>
-  $(function(){
-      //get the pie chart canvas
-      var prod = JSON.parse(`<?php echo $chart_data_prod; ?>`);
-      var plan = JSON.parse(`<?php echo $chart_data_plan; ?>`);
-      var ctx = $("#pie-chart");
- 
-    //pie chart data
-    var data = {
-        labels: prod.label,
-        datasets: [{
-            type: 'bar',
-            label: 'Overburden',
-            data: prod.data,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)'
-        }, {
-            type: 'line',
-            label: 'Plan',
-            data: plan.data,
-            fill: false,
-            borderColor: 'rgb(54, 162, 235)'
-        }]
-      };
- 
-      //options
-      var options = {
-        responsive: true,
-        title: {
-          display: true,
-          position: "top",
-          text: "",
-          fontSize: 18,
-          fontColor: "#111"
-        },
-        legend: {
-          display: true,
-          position: "bottom",
-          labels: {
-            fontColor: "#333",
-            fontSize: 16
-          }
-        }
-      };
- 
-    //   Create Mixed Chart
-      var chart1 = new Chart(ctx, {
-        type: "bar",
-        data: data,
-        options: options
-      });
- 
-  });
+    $(function() {
+        // OVERBURDEN
+        //get the OB data
+        var ob_prod = JSON.parse(`<?php echo $data_prod_ob['chart_data_prod_ob']; ?>`);
+        var ob_plan = JSON.parse(`<?php echo $data_plan_ob['chart_data_plan_ob']; ?>`);
+        var ctx = $("#overburden");
+
+        //Multi Chart
+        var data = {
+            labels: ob_prod.label,
+            datasets: [{
+                type: 'bar',
+                label: 'Overburden',
+                data: ob_prod.data,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)'
+            }, {
+                type: 'line',
+                label: 'Plan',
+                data: ob_plan.data,
+                fill: false,
+                borderColor: 'rgb(54, 162, 235)'
+            }]
+        };
+
+        //options
+        var options = {
+            responsive: true,
+            title: {
+                display: true,
+                position: "top",
+                text: "",
+                fontSize: 18,
+                fontColor: "#111"
+            },
+            legend: {
+                display: true,
+                position: "bottom",
+                labels: {
+                    fontColor: "#333",
+                    fontSize: 16
+                }
+            }
+        };
+
+        //   Create Mixed Chart
+        var chart1 = new Chart(ctx, {
+            type: "bar",
+            data: data,
+            options: options
+        });
+
+        // Coal
+        //get the Coal data
+        var coal_prod = JSON.parse(`<?php echo $data_prod_coal['chart_data_prod_coal']; ?>`);
+        var coal_plan = JSON.parse(`<?php echo $data_plan_coal['chart_data_plan_coal']; ?>`);
+        var ctx = $("#coal");
+
+        //Multi Chart
+        var data = {
+            labels: coal_prod.label,
+            datasets: [{
+                type: 'bar',
+                label: 'Overburden',
+                data: coal_prod.data,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)'
+            }, {
+                type: 'line',
+                label: 'Plan',
+                data: coal_plan.data,
+                fill: false,
+                borderColor: 'rgb(54, 162, 235)'
+            }]
+        };
+
+        //options
+        var options = {
+            responsive: true,
+            title: {
+                display: true,
+                position: "top",
+                text: "",
+                fontSize: 18,
+                fontColor: "#111"
+            },
+            legend: {
+                display: true,
+                position: "bottom",
+                labels: {
+                    fontColor: "#333",
+                    fontSize: 16
+                }
+            }
+        };
+
+        //   Create Mixed Chart
+        var chart2 = new Chart(ctx, {
+            type: "bar",
+            data: data,
+            options: options
+        });
+
+    });
 </script>
 @endsection
