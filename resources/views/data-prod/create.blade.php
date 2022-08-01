218e67eca06a9bcf5c405b56ca1f4f4a6cb5221b
TOGICANDRA2734 <togicandra@gmail.com>
2022-08-01 13:33:16 +0800

    Test

--- resources/views/data-prod/create.blade.php
+++ resources/views/data-prod/create.blade.php
@@ -16,61 +16,40 @@
             <form action="#" method="POST" id="storeDok" class="px-4 py-3 mb-8 grid grid-cols-2 gap-5 bg-white rounded-lg  dark:bg-gray-800">
                 <input type="hidden" name="_token" value="bXIClGN8zmzviC81dlbhBacKfOAI5MS2T4ECFGqy"> 
                 <label class="block mt-4 text-sm">
-                    <span class="font-semibold text-gray-700 dark:text-gray-400">Unit</span>
-                    <select class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" name="id_tiket" id="id_tiket">
-                        <option value="1">1 - KD37013</option>
-                        <option value="2">2 - KD37008</option>
-                        <option value="47">47 - KD37012</option>
-                        <option value="48">48 - KD37012</option>
-                        <option value="49">49 - MR40028</option>
-                        <option value="50">50 - KD37031</option>
+                    <span class="font-semibold text-gray-700 dark:text-gray-400">Tanggal Data</span>
+                        <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="date" name="tgl" id="tgl">
                     </select>
                 </label>
 
                 <label class="block mt-4 text-sm">
-                    <span class="font-semibold text-gray-700 dark:text-gray-400">Dokumen</span>
-                    <select class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" name="dok_type" id="dok_type">
+                    <span class="font-semibold text-gray-700 dark:text-gray-400">Site</span>
+                    <select class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" name="site" id="site">
                         <option value="SR">SR</option>
-                        <option value="RS">RS</option>
-                        <option value="KO">KO</option>
                     </select>
                 </label>
 
-
                 <label class="block mt-4 text-sm">
-                    <span class="font-semibold text-gray-700 dark:text-gray-400">
-                        No
-                    </span>
-                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="text" name="dok_no" id="dok_no">
+                    <span class="font-semibold text-gray-700 dark:text-gray-400">Pit</span>
+                    <select class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" name="pit" id="pit">
+                        <option value="SR">SR</option>
+                    </select>
                 </label>
 
                 <label class="block mt-4 text-sm">
                     <span class="font-semibold text-gray-700 dark:text-gray-400">
-                        Tanggal
+                        Overburden (bcm)
                     </span>
-                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="date" name="dok_tgl" id="dok_tgl">
+                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="number" name="ob" id="ob">
                 </label>
+
 
                 <label class="block mt-4 text-sm">
                     <span class="font-semibold text-gray-700 dark:text-gray-400">
-                        Deskripsi
+                        Coal (mt)
                     </span>
-                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="text" name="uraian" id="uraian">
+                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="number" name="ob" id="ob">
                 </label>
 
-                <label class="block mt-4 text-sm">
-                    <span class="font-semibold text-gray-700 dark:text-gray-400">
-                        Keterangan
-                    </span>
-                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="text" name="keterangan" id="keterangan">
-                </label>
-
-                <label class="block mt-4 text-sm">
-                    <span class="font-semibold text-gray-700 dark:text-gray-400">
-                        Uraian Breakdown
-                    </span>
-                    <input class="block shadow-sm border p-2 rounded-md w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-multiselect focus:border-stone-400 focus:outline-none focus:shadow-outline-stone dark:focus:shadow-outline-gray" type="text" name="uraian_bd" id="uraian_bd">
-                </label>
 
                 <button type="submit" class="px-5 py-3 mt-4 col-span-2 font-medium leading-5 text-white transition-colors duration-150 bg-stone-700 border border-transparent rounded-lg active:bg-stone-600 hover:bg-stone-900 focus:outline-none focus:shadow-outline-stone w-full">Submit</button>
             </form>
